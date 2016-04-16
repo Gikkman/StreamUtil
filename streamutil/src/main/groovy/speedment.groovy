4ffdd0e0913acf8dda04b5300381df56f9ff6198
Gikkman <ksr.jonasson@gmail.com>
2016-04-16 12:14:01 +0200

    Initial code commit (hope it works this time)
    This is the mere basics. Some test code, to make sure stuff work as
    intended.

---
+++ streamutil/src/main/groovy/speedment.groovy
@@ -0,0 +1,141 @@
+import com.speedment.config.parameters.*
+
+name = "gikk_stream_util";
+packageLocation = "src/main/java";
+packageName = "com.company.speedment.test";
+enabled = true;
+expanded = true;
+dbms {
+    ipAddress = "127.0.0.1";
+    name = "db0";
+    port = 3306;
+    typeName = "MySQL";
+    username = "gikkman";
+    enabled = true;
+    expanded = true;
+    schema {
+        columnCompressionType = ColumnCompressionType.NONE;
+        fieldStorageType = FieldStorageType.WRAPPER;
+        name = "gikk_stream_util";
+        schemaName = "gikk_stream_util";
+        storageEngineType = StorageEngineType.ON_HEAP;
+        defaultSchema = false;
+        enabled = true;
+        expanded = true;
+        table {
+            columnCompressionType = ColumnCompressionType.INHERIT;
+            fieldStorageType = FieldStorageType.INHERIT;
+            name = "users";
+            storageEngineType = StorageEngineType.INHERIT;
+            tableName = "users";
+            enabled = true;
+            expanded = true;
+            column {
+                columnCompressionType = ColumnCompressionType.INHERIT;
+                databaseType = java.lang.Integer.class;
+                fieldStorageType = FieldStorageType.INHERIT;
+                name = "ID";
+                typeMapper = com.speedment.internal.core.config.mapper.identity.IntegerIdentityMapper.class;
+                autoincrement = true;
+                enabled = true;
+                expanded = true;
+                nullable = false;
+            }
+            column {
+                columnCompressionType = ColumnCompressionType.INHERIT;
+                databaseType = java.lang.String.class;
+                fieldStorageType = FieldStorageType.INHERIT;
+                name = "USERNAME";
+                typeMapper = com.speedment.internal.core.config.mapper.identity.StringIdentityMapper.class;
+                autoincrement = false;
+                enabled = true;
+                expanded = true;
+                nullable = false;
+            }
+            column {
+                columnCompressionType = ColumnCompressionType.INHERIT;
+                databaseType = java.lang.Integer.class;
+                fieldStorageType = FieldStorageType.INHERIT;
+                name = "TIME_ONLINE";
+                typeMapper = com.speedment.internal.core.config.mapper.identity.IntegerIdentityMapper.class;
+                autoincrement = false;
+                enabled = true;
+                expanded = true;
+                nullable = false;
+            }
+            column {
+                columnCompressionType = ColumnCompressionType.INHERIT;
+                databaseType = java.lang.Integer.class;
+                fieldStorageType = FieldStorageType.INHERIT;
+                name = "LINES_WRITTEN";
+                typeMapper = com.speedment.internal.core.config.mapper.identity.IntegerIdentityMapper.class;
+                autoincrement = false;
+                enabled = true;
+                expanded = true;
+                nullable = false;
+            }
+            column {
+                columnCompressionType = ColumnCompressionType.INHERIT;
+                databaseType = java.lang.String.class;
+                fieldStorageType = FieldStorageType.INHERIT;
+                name = "IS_TRUSTED";
+                typeMapper = com.speedment.internal.core.config.mapper.string.TrueFalseStringToBooleanMapper.class;
+                autoincrement = false;
+                enabled = true;
+                expanded = true;
+                nullable = false;
+            }
+            column {
+                columnCompressionType = ColumnCompressionType.INHERIT;
+                databaseType = java.lang.String.class;
+                fieldStorageType = FieldStorageType.INHERIT;
+                name = "IS_FOLLOWER";
+                typeMapper = com.speedment.internal.core.config.mapper.string.TrueFalseStringToBooleanMapper.class;
+                autoincrement = false;
+                enabled = true;
+                expanded = true;
+                nullable = false;
+            }
+            column {
+                columnCompressionType = ColumnCompressionType.INHERIT;
+                databaseType = java.lang.String.class;
+                fieldStorageType = FieldStorageType.INHERIT;
+                name = "IS_SUBSCRIBER";
+                typeMapper = com.speedment.internal.core.config.mapper.string.TrueFalseStringToBooleanMapper.class;
+                autoincrement = false;
+                enabled = true;
+                expanded = true;
+                nullable = false;
+            }
+            primaryKeyColumn {
+                name = "ID";
+                enabled = true;
+                expanded = true;
+            }
+            index {
+                name = "PRIMARY";
+                enabled = true;
+                expanded = false;
+                unique = true;
+                indexColumn {
+                    name = "ID";
+                    orderType = OrderType.ASC;
+                    enabled = true;
+                    expanded = true;
+                }
+            }
+            index {
+                name = "USERNAME";
+                enabled = true;
+                expanded = false;
+                unique = true;
+                indexColumn {
+                    name = "USERNAME";
+                    orderType = OrderType.ASC;
+                    enabled = true;
+                    expanded = true;
+                }
+            }
+        }
+    }
+}
---
+++ streamutil/src/main/java/com/gikk/streamutil/Main.java
@@ -0,0 +1,88 @@
+package com.gikk.streamutil;
+
+
+
+import com.gikk.speedment.test.gikk_stream_util.GikkStreamUtilApplication;
+import com.gikk.speedment.test.gikk_stream_util.db0.gikk_stream_util.users.Users;
+import com.speedment.Manager;
+import com.speedment.Speedment;
+import com.speedment.exception.SpeedmentException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Your name
+ */
+public class Main extends Application{
+	
+	@FXML TextField txt_field;
+	
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene (FXMLLoader.load( getClass().getResource("Main.fxml") ) );
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+        
+        System.out.println();
+        
+        
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@FXML protected void onClick(ActionEvent e){
+		addUser( txt_field.getText() );
+	}
+	
+	private void addUser(String name){
+		Speedment speedment = new GikkStreamUtilApplication().build();
+        Manager<Users> users = speedment.managerOf(Users.class);
+        
+        final String lcName = name.toLowerCase();
+        
+        if( users.stream().parallel().filter( p -> p.getUsername().matches(lcName) ).count() > 0 ){
+        	System.out.println("User already present");
+        	return;
+        }
+        
+		Thread t = new Thread( () -> {
+	        try {
+	            Users user = users.newInstance()
+	                .setUsername(lcName)
+	                .setIsFollower(false)
+	                .setIsSubscriber(false)
+	                .setIsTrusted(false)
+	                .setLinesWritten(0)
+	                .setTimeOnline(0)
+	                .persist();
+	                
+	            System.out.print("Hello nr. " + user.getId() +", "
+	            		+ "Name: " 		  	+ user.getUsername() + ", "
+	            		+ "Time online: " 	+ user.getTimeOnline() +", "
+	            		+ "Lines written: " + user.getLinesWritten() +", "
+	            		+ "Is trusted: " 	+ user.getIsTrusted() +", "
+	            		+ "Is follower: " 	+ user.getIsFollower() +", "
+	            		+ "Is subscriber: " + user.getIsSubscriber() );
+	        } catch (SpeedmentException se) {
+	           System.out.println("Why are you so persistant?");
+	           System.err.println( se.getLocalizedMessage() );
+	        } } );
+        t.start();
+	}
+}
